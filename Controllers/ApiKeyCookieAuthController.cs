1115df9ca5afc92e7d67274cf615fad8845e05dd
Barış Gül <barisgul@barss-mbp.home>
2026-05-11 18:50:08 +0200

    fix

--- Controllers/ApiKeyCookieAuthController.cs
+++ Controllers/ApiKeyCookieAuthController.cs
@@ -7,11 +7,13 @@ namespace AuthSandbox.Controllers;
 [Route("api/[controller]")]
 [Authorize(AuthenticationSchemes = "ApiKeyCookie")]
 [ApiExplorerSettings(GroupName = "v1-apikey-cookie")]
+
 public class ApiKeyCookieAuthController : ControllerBase
 {
     [HttpGet("secure-data")]
     public IActionResult GetSecureData()
     {
         return Ok(new { Message = "This data is secured by API Key (Cookie) authentication.", User = User.Identity?.Name });
+        
     }
 }
